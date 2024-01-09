cb181d3951212d38db67bb0db34daf55c53baa7c
Jacky <137807502+JackyHuth@users.noreply.github.com>
2024-01-09 15:31:48 +0100

    Update SongAnalyzer.java

--- src/business/game/SongAnalyzer.java
+++ src/business/game/SongAnalyzer.java
@@ -1,5 +1,24 @@
 package business.game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public abstract class SongAnalyzer {
 
+	
+	public void analyzeSong() {
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/music/"+ name + ".m3u"))) {
+//            Playlist playlist = new Playlist(name);
+//       }     String readLine = bufferedReader.readLine();
+//            while (readLine != null) {
+//                Track track = new Track(readLine);
+//                playlist.addTrack(track);
+//                readLine = bufferedReader.readLine();
+         
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }	
+	}
+	
 }
